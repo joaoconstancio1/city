7f627def08cf2af08127b90e11874e8c5c8d538b
joao vitor <joaovitorconstancio@hotmail.com>
2024-11-08 15:03:01 -0300

    Add WeatherCard component and update HomePage to display weather information

--- lib/features/home/presentation/pages/home_page.dart
+++ lib/features/home/presentation/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:city/core/custom_error_widget.dart';
+import 'package:city/features/home/presentation/components/weather_card.dart';
 import 'package:city/features/home/presentation/cubit/home_page_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -9,9 +10,12 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(providers: [
-      BlocProvider(create: (_) => HomePageCubit(Modular.get())..init()),
-    ], child: HomePageView());
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => HomePageCubit(Modular.get())..init()),
+      ],
+      child: HomePageView(),
+    );
   }
 }
 
@@ -26,31 +30,32 @@ class _HomePageViewState extends State<HomePageView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Home Page'),
-          centerTitle: true,
-        ),
-        body: BlocBuilder<HomePageCubit, HomePageState>(
-          builder: (context, state) {
-            if (state is HomePageLoadingState) {
-              return const Center(child: CircularProgressIndicator());
-            }
-            if (state is HomePageErrorState) {
-              return CustomErrorWidget(
-                message: 'Something went wrong. Please try again.',
-                onRetry: () {
-                  Modular.get<HomePageCubit>().init();
-                },
-              );
-            }
+      appBar: AppBar(
+        title: Text('Weather Info'),
+        centerTitle: true,
+      ),
+      body: BlocBuilder<HomePageCubit, HomePageState>(
+        builder: (context, state) {
+          if (state is HomePageLoadingState) {
+            return const Center(child: CircularProgressIndicator());
+          }
 
-            if (state is HomePageSuccessState) {
-              return Card(
-                child: Text(state.cities.first.description ?? ''),
-              );
-            }
-            return SizedBox();
-          },
-        ));
+          if (state is HomePageErrorState) {
+            return CustomErrorWidget(
+              message: 'Something went wrong. Please try again.',
+              onRetry: () {
+                Modular.get<HomePageCubit>().init();
+              },
+            );
+          }
+
+          if (state is HomePageSuccessState) {
+            return WeatherCard(data: state.cities);
+          }
+
+          return SizedBox();
+        },
+      ),
+    );
   }
 }
