ed4d743d5ee291ca15fb0d775241e5f705c6c1d9
joao vitor <joaovitorconstancio@hotmail.com>
2024-11-08 08:31:02 -0300

    Add initial Flutter app structure with modular routing and VSCode configuration

---
+++ lib/app_module.dart
@@ -0,0 +1,12 @@
+import 'package:city/home_page.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+class AppModule extends Module {
+  @override
+  void binds(i) {}
+
+  @override
+  void routes(r) {
+    r.child('/', child: (context) => HomePage());
+  }
+}
