57a2602a79c06fae676ba39987eb000a93d0bb92
joao vitor <joaovitorconstancio@hotmail.com>
2024-11-09 17:27:33 -0300

    Refactor HomeModule to use AppModule for dependency injection; streamline bindings and routes

--- lib/features/home/home_module.dart
+++ lib/features/home/home_module.dart
@@ -1,22 +1,20 @@
-import 'package:city/core/dio_http_client.dart';
-import 'package:city/core/custom_http_client.dart';
+import 'package:city/app_module.dart';
 import 'package:city/features/home/data/datasources/home_datasource.dart';
 import 'package:city/features/home/data/repositories/home_repository_impl.dart';
 import 'package:city/features/home/domain/repositories/home_repository.dart';
 import 'package:city/features/home/presentation/cubit/home_page_cubit.dart';
 import 'package:city/features/home/presentation/pages/edit_or_createpage.dart';
 import 'package:city/features/home/presentation/pages/home_page.dart';
-import 'package:dio/dio.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class HomeModule extends Module {
   @override
+  List<Module> get imports => [
+        AppModule(),
+      ];
+
+  @override
   void binds(i) {
-    final dio = Dio(BaseOptions());
-
-    i.addSingleton<Dio>(() => dio);
-
-    i.addSingleton<CustomHttpClient>(DioHttpClient.new);
     i.add<HomeRepository>(HomeRepositoryImpl.new);
     i.add<HomeDatasource>(HomeDatasourceImpl.new);
     i.add(HomePageCubit.new);
@@ -25,9 +23,6 @@ class HomeModule extends Module {
   @override
   void routes(r) {
     r.child('/', child: (context) => HomePage());
-    r.child('/edit',
-        child: (context) => EditOrCreatePage(
-              city: r.args.data,
-            ));
+    r.child('/edit', child: (context) => EditOrCreatePage(city: r.args.data));
   }
 }
