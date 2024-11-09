8a2d65904d34898603b55466c8223f2e476333fa
joao vitor <joaovitorconstancio@hotmail.com>
2024-11-09 17:42:23 -0300

    Remove redundant title getter from F class in flavors.dart; simplify flavor management

--- lib/flavors.dart
+++ lib/flavors.dart
@@ -7,21 +7,6 @@ enum Flavor {
 class F {
   static Flavor? appFlavor;
 
-  static String get name => appFlavor?.name ?? '';
-
-  static String get title {
-    switch (appFlavor) {
-      case Flavor.dev:
-        return 'City dev';
-      case Flavor.stg:
-        return 'City stg';
-      case Flavor.prod:
-        return 'City';
-      default:
-        return 'title';
-    }
-  }
-
   static String get baseUrl {
     switch (appFlavor) {
       case Flavor.dev:
