32349663494d5c76f4f895a9414713f331bc12d1
joao vitor <joaovitorconstancio@hotmail.com>
2024-11-10 20:30:44 -0300

    Enhance HomePage tests: add navigation and delete confirmation dialog tests

--- test/features/home/presentation/pages/home_page_test.dart
+++ test/features/home/presentation/pages/home_page_test.dart
@@ -1,4 +1,5 @@
 import 'package:bloc_test/bloc_test.dart';
+import 'package:city/app_module.dart';
 import 'package:city/core/core_extensions.dart';
 import 'package:city/core/custom_error_widget.dart';
 import 'package:city/core/custom_loading_widget.dart';
@@ -8,35 +9,39 @@ import 'package:city/features/home/presentation/cubit/home_page_cubit.dart';
 import 'package:city/features/home/presentation/cubit/home_page_states.dart';
 import 'package:city/features/home/presentation/pages/home_page.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 
 class MockHomePageCubit extends Mock implements HomePageCubit {}
 
+class ModularNavigateMock extends Mock implements IModularNavigator {}
+
 void main() {
   late MockHomePageCubit mockHomePageCubit;
+  final navigator = ModularNavigateMock();
+  Modular.navigatorDelegate = navigator;
 
   setUp(() {
     mockHomePageCubit = MockHomePageCubit();
+    Modular.navigatorDelegate = navigator;
+    Modular.bindModule(AppModule());
+    Modular.replaceInstance<HomePageCubit>(mockHomePageCubit);
 
     when(() => mockHomePageCubit.close()).thenAnswer((_) async {});
 
+    // Setup initial states for the cubit
     final cities = [
       CityEntity(
           id: '1', city: 'City 1', temperature: '2', description: 'sunny'),
       CityEntity(
-          id: '2', city: 'City 2', temperature: '12', description: 'rainning'),
+          id: '2', city: 'City 2', temperature: '12', description: 'raining'),
     ];
     when(() => mockHomePageCubit.state)
         .thenReturn(HomePageSuccessState(cities: cities));
-
     whenListen(
-      mockHomePageCubit,
-      Stream<HomePageState>.fromIterable(
-        [HomePageSuccessState(cities: cities)],
-      ),
-    );
+        mockHomePageCubit,
+        Stream<HomePageState>.fromIterable(
+            [HomePageSuccessState(cities: cities)]));
   });
 
   Widget createTestableWidget() {
@@ -133,5 +138,55 @@ void main() {
     await tester.pumpAndSettle();
 
     verify(() => mockHomePageCubit.init()).called(1);
+  });
+
+  testWidgets('calls init when returning from edit page',
+      (WidgetTester tester) async {
+    when(() => navigator.pushNamed(any())).thenAnswer((_) async => null);
+    when(() => mockHomePageCubit.init()).thenAnswer((_) async {});
+
+    await tester.pumpWidget(createTestableWidget());
+
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pumpAndSettle();
+
+    verify(() => navigator.pushNamed('/edit')).called(1);
+    verify(() => mockHomePageCubit.init()).called(1);
+  });
+
+  testWidgets(
+      'shows delete confirmation dialog when delete action is triggered',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(createTestableWidget());
+
+    await tester.tap(find.descendant(
+      of: find.widgetWithText(WeatherCard, 'City 1'),
+      matching: find.byIcon(Icons.delete),
+    ));
+    await tester.pumpAndSettle();
+
+    expect(
+        find.text(
+            'Are you sure you want to delete this city? This action cannot be undone.'),
+        findsOneWidget);
+    expect(find.text('Delete'), findsOneWidget);
+    expect(find.text('Cancel'), findsOneWidget);
+  });
+
+  testWidgets('closes delete confirmation dialog when cancel is pressed',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(createTestableWidget());
+
+    await tester.tap(find.descendant(
+      of: find.widgetWithText(WeatherCard, 'City 1'),
+      matching: find.byIcon(Icons.delete),
+    ));
+    await tester.pumpAndSettle();
+
+    await tester.tap(find.text('Cancel'));
+    await tester.pumpAndSettle();
+
+    expect(
+        find.text('Are you sure you want to delete this city?'), findsNothing);
   });
 }
